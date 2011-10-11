64cc41bae2066c8842db9607ca792b06cc68a3c8
William Strecker-Kellogg <willsk@bnl.gov>
2011-10-10 23:32:06 -0400

    Working with range object

--- rsort.c
+++ rsort.c
@@ -11,10 +11,7 @@ static void _swp(void *a, void *b, size_t s)
     unsigned char t;
     size_t c = 0;
 
-    printf("Swap %p (%d) and %p (%d)\n", a, *(int *)a, b, *(int *)b);
-
     do {
-//        printf("%d Swap %x / %x: %x %x\n", c, p, q, *p, *q);
         t = *(unsigned char *)(a + c);
         *(unsigned char *)(a + c) = *(unsigned char *)(b + c);
         *(unsigned char *)(b + c) = t;
@@ -27,12 +24,10 @@ int part(void *p, size_t s, int low, int high, int (*cmp)(void *, void *))
     int i, j;
     int piv = (high + low) / 2;
 
-    printf("Pivot: %d (%d)\n", piv, *(int *)(p + piv * s));
-
     _swp((p + s * piv), (p + s * (high)), s);
     k = (p + s * (high));
-    j = low;
-    for(i = low; i < high; i++) {
+
+    for(i = j = low; i < high; i++) {
         if(cmp((p + s * i), k) <= 0)   {
             _swp((p + s * i), (p + s * j), s);
             j++;
@@ -42,26 +37,13 @@ int part(void *p, size_t s, int low, int high, int (*cmp)(void *, void *))
     return j;
 }
 
-int _qs(void *p, size_t s, int low, int high, int (*cmp)(void *, void *))
+void _qs(void *p, size_t s, int low, int high, int (*cmp)(void *, void *))
 {
-    int i;
-    printf("Low: %d, High: %d\n", low, high);
     if(low < high)  {
-        int pivot;
-        //
-        printf("\nBefore Partitioning:\n");
-        for(i = low; i < high; i++)      {
-            printf("(%2d) %p: %d\n", i, (int *)(p + i * s), *(int *)(p + i * s));
-        }
-        pivot = part(p, s, low, high, cmp);
-        printf("\nAfter Partitioning, np=%d:\n", pivot);
-        for(i = low; i < high; i++)      {
-            printf("(%2d) %p: %d\n", i, (int *)(p + i * s), *(int *)(p + i * s));
-        }
+        int pivot = part(p, s, low, high, cmp);
         _qs(p, s, low, pivot - 1, cmp);
         _qs(p, s, pivot + 1, high, cmp);
     }
-    return 0;
 }
 
 
@@ -114,7 +96,6 @@ int icmp(void *a, void *b)
 {
     int *i = (int *)a;
     int *j = (int *)b;
-    printf("Compare: %d w/ %d\n", *i, *j);
     if(*i < *j)
         return -1;
     else if(*i == *j)
@@ -123,45 +104,34 @@ int icmp(void *a, void *b)
         return 1;
 }
 
-#define NUM 20
+#define NUM 60
 
 int main(int argc, char *argv[])
 {
-    struct range r[] = { {4,8}, {2,6}, {40,43}, {30,111}, {322,334}, {11,121} };
+    //struct range r[] = { {4,8}, {2,6}, {31,43}, {30,111}, {322,334}, {11,121} };
+    struct range r[NUM] = {0};
     //int ints[] = {21, 2, 11, 33, 20, 19, 10, 6, 8, 12, 29, 32, 13};
     int ints[NUM];
-    //struct range r[30];
     int i;
 
-    srand((unsigned)time(NULL) + getpid());
-    //srand(23212);
-    for(i = 0; i < NUM; i++)      {
-        //printf("%p: %d\n", &ints[i], ints[i]);
-        ints[i] = rand() % 100;
-        printf("%d ", ints[i]);
-    }
-
     //_swp(&ints[8], &ints[4], sizeof(int));
-    qsrt(ints, sizeof(int), NUM, icmp);
+    //qsrt(ints, sizeof(int), NUM, icmp);
 
-    printf("\n---------------------\n");
-
+    srand(time(NULL) ^ getpid());
     for(i = 0; i < NUM; i++)      {
-        printf("%p: %d\n", &ints[i], ints[i]);
-    }
-/*
-    for(i = 0; i < 6; i++)      {
+        r[i].lo = rand() % 100;
+        r[i].hi = r[i].lo + rand() % 100;
         printf("%p: %d-%d\n", &r[i], r[i].lo, r[i].hi);
     }
 
     //swp(&r[atoi(argv[1])], &r[atoi(argv[2])], sizeof(struct range));
-    qsrt(r, sizeof(struct range), 6, rcmp);
+    qsrt(r, sizeof(struct range), NUM, rcmp);
 
     printf("\n---------------------\n");
 
-    for(i = 0; i < 6; i++)      {
+    for(i = 0; i < NUM; i++)      {
         printf("%p: %d-%d\n", &r[i], r[i].lo, r[i].hi);
-    }*/
+    }
 
 //    qsrt(r, 6, sizeof(struct range), rcmp);
 
