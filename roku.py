c8b7510fbcfc9fff9e76d234d40ad4df5647f34e
William Strecker-Kellogg <willsk@bnl.gov>
2014-10-02 21:14:39 -0400

    roku: fully lint compliant

--- roku.py
+++ roku.py
@@ -4,30 +4,34 @@ import time
 import requests
 from xml.etree import ElementTree
 
+
 class RokuApp(object):
     def __init__(self, app_id, name, version, remote=None):
         self.id = app_id
         self.name = name
         self.version = version
         self._remote = remote
+
     def __repr__(self):
         cls = self.__class__.__name__
         values = {k: repr(v) for k, v in self.__dict__.iteritems()}
         return "{0}({id}, {name}, {version})".format(cls, **values)
+
     def launch(self):
         self._remote.launch(self.id)
+
 
 class RokuRemote(object):
     def __init__(self, location):
         self.location = location
-    
+
     def __repr__(self):
         return "{0}({1})".format(self.__class__.__name__, repr(self.location))
-    
+
     def _call(self, method, path, params=None):
         url = "".join([self.location, path])
         return requests.request(method, url, params=params).text
-    
+
     def keypress(self, key, count=1, delay=0.05):
         for x in xrange(count):
             self._call("post", "/keypress/{0}".format(key))
@@ -50,7 +54,7 @@ class RokuRemote(object):
 
     def keyup(self, key):
         self._call("post", "/keyup/{0}".format(key))
-    
+
     def query_apps(self):
         apps_xml = ElementTree.fromstring(self._call("get", "/query/apps"))
         apps = {}
@@ -61,10 +65,10 @@ class RokuRemote(object):
 
     def launch(self, app, params=None):
         self._call("post", "/launch/{0}".format(app), params=params)
-    
+
     def send_input(self, params):
         self._call("post", "/input", params=params)
-    
+
     def type_string(self, string):
         for letter in string:
             self.keypress("Lit_{0}".format(letter))
@@ -86,6 +90,7 @@ def best_move(o, n, ref):
     if abs(dist) > size / 2:
         dist = (abs(dist) - len(ref)) * sign(dist)
     return dist
+
 
 def mycompare(o, n, set):
     return set.index(n) - set.index(o)
@@ -116,7 +121,7 @@ def set_position(o, n, r):
         move(dist)
 
 
-def youtube_type(roku, string, starting_key = 'a'):
+def youtube_type(roku, string, starting_key='a'):
     last = starting_key
     for char in string.strip():
         set_position(last, char, roku)
@@ -127,4 +132,3 @@ def youtube_type(roku, string, starting_key = 'a'):
 roku = RokuRemote('http://192.168.1.128:8060')
 
 youtube_type(roku, raw_input('Search: '))
-
